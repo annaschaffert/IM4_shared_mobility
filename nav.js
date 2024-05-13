a7cfc8ee426251911786ceb6f82ba83d6c58eb15
adinadarms <adina.darms@stud.fhgr.ch>
2024-05-13 09:28:41 +0200

    adina nav

--- nav.js
+++ nav.js
@@ -38,8 +38,44 @@ async function fetchData() {
     }
 }
 async function main() {
-    let data = await fetchData();  
-    console.log(data);
+    let data = await fetchData(); 
+    let time = data.time;
+    let weekday = data.weekday;
+    let total_num_vehicles = data.total_num_vehicles; 
+
+const ctx = document.getElementById("myChart").getContext("2d");
+
+const myChart = {
+    labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+    datasets: [
+        {
+            label: "Total Number of Vehicles",
+            data: total_num_vehicles,
+            backgroundColor: "rgba(54, 162, 235, 0.2)",
+            borderColor: "rgba(54, 162, 235, 1)",
+            borderWidth: 1,
+        },
+    ],
+};
+
+const options = {
+    scale: {
+        ticks: {
+            beginAtZero: true,
+            max: Math.max(...total_num_vehicles) + 10,
+        },
+    },
+};
+
+const radarChart = new Chart(ctx, {
+    type: "radar",
+    data: data,
+    options: options,
+});
+
 }
+
+
+console.log(data);
 
 main();
