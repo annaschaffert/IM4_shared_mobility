240bc0051a178cab75636bc50af3c394f4f3172d
adinadarms <adina.darms@stud.fhgr.ch>
2024-05-14 15:36:18 +0200

    adina

--- mobility.php
+++ mobility.php
@@ -1,40 +1,44 @@
 <?php
 
-require_once 'config.php';
-
-try {
-    $pdo = new PDO($dsn, $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    $query = "SELECT time, DAYNAME(time) AS weekday, SUM(station_status_num_vehicle_available) AS total_num_vehicles FROM Shared_Mobility GROUP BY time";
-    $stmt = $pdo->prepare($query);
-    $stmt->execute();
-
-    $data = []; // Initialize an empty array to store the data
-
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        $time = $row['time'];
-        $weekday = $row['weekday'];
-        $totalNumVehicles = $row['total_num_vehicles'];
-
-        // Create an associative array with the desired structure
-        $entry = [
-            'weekday' => $weekday,
-            'time' => $time,
-            'total_num_vehicles' => $totalNumVehicles
-        ];
-
-        // Add the entry to the data array
-        $data[] = $entry;
+function randomTotalNumVehicles() {
+    $total_num_vehicles = [];
+    for ($i = 0; $i < 24; $i++) {
+        $randomNumber;
+        if ($i >= 0 && $i < 9) {
+            // Zufallszahlen zwischen 30 und 40 für die Stunden von 0 bis 9 Uhr
+            $randomNumber = rand(30, 40);
+        } else if ($i >= 9 && $i < 17) {
+            // Zufallszahlen zwischen 10 und 30 für die Stunden von 10 bis 16 Uhr
+            $randomNumber = rand(10, 30);
+        } else {
+            // Zufallszahlen zwischen 2 und 40 für die Stunden von 17 bis 23 Uhr
+            $randomNumber = rand(2, 40);
+        }
+        array_push($total_num_vehicles, $randomNumber);
     }
-
-    // Convert the data array to JSON
-    $jsonData = json_encode($data);
-
-    // Do something with the JSON data
-    // For example, you can echo it
-    echo $jsonData;
-} catch (PDOException $e) {
-    echo "Error: " . $e->getMessage();
+    return $total_num_vehicles;
 }
 
+// Generate random data for each day
+$data = [];
+$weekdays = ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"];
+foreach ($weekdays as $weekday) {
+    // $totalNumVehicles = $row['total_num_vehicles'];
+    // Alternative wegen instabilder API, genehmigt von Samuel Rhyner
+    $totalNumVehicles = randomTotalNumVehicles();
+    $entry = [
+        'weekday' => $weekday,
+        'total_num_vehicles' => $totalNumVehicles
+    ];
+    array_push($data, $entry);
+}
+
+// Convert the data array to JSON
+$jsonData = json_encode($data);
+
+// Set the content type to JSON
+header('Content-Type: application/json');
+
+// Output the JSON data
+echo $jsonData;
+?>
